93f8d61f8673a876ef2129e70f6c0ee12bb21a3c
Nick Smallbone <nick.smallbone@gmail.com>
2015-04-15 16:48:55 +0200

    Rejig the PrettyTerm class.

--- src/KBC/Pretty.hs
+++ src/KBC/Pretty.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE CPP #-}
+-- | Pretty-printing of terms and assorted other values.
+
+{-# LANGUAGE CPP, Rank2Types #-}
 module KBC.Pretty where
 
 #include "errors.h"
@@ -11,17 +13,19 @@ import Data.Ratio
 import Data.Rewriting.Rule(Rule(..))
 import Data.Rewriting.Term(Term(..))
 
-instance Pretty Doc where pPrint = id
+-- * Miscellaneous 'Pretty' instances and utilities.
 
-pPrintTuple :: [Doc] -> Doc
-pPrintTuple = parens . fsep . punctuate comma
+prettyPrint :: Pretty a => a -> IO ()
+prettyPrint x = putStrLn (prettyShow x)
 
 pPrintParen :: Bool -> Doc -> Doc
 pPrintParen True  d = parens d
 pPrintParen False d = d
 
-prettyPrint :: Pretty a => a -> IO ()
-prettyPrint x = putStrLn (prettyShow x)
+instance Pretty Doc where pPrint = id
+
+pPrintTuple :: [Doc] -> Doc
+pPrintTuple = parens . fsep . punctuate comma
 
 instance Pretty a => Pretty (Set a) where
   pPrint = pPrintSet . map pPrint . Set.toList
@@ -39,87 +43,109 @@ instance (Eq a, Integral a, Pretty a) => Pretty (Ratio a) where
     | denominator a == 1 = pPrint (numerator a)
     | otherwise = text "(" <+> pPrint a <+> text ")"
 
-class Pretty a => PrettyTerm a where
-  termStyle :: a -> TermStyle
-  termStyle _ = Curried
-
-data TermStyle = Invisible | Curried | Uncurried | Tuple Int | TupleType | ListType | Infix Int | Infixr Int | Prefix | Postfix | Gyrator deriving Show
-
-instance (PrettyTerm f, Pretty v) => Pretty (Term f v) where
-  pPrintPrec l p (Var x) = pPrintPrec l p x
-  pPrintPrec l p (Fun f xs) =
-    pPrintStyle (termStyle f) l p (pPrint f) xs
-
 instance (PrettyTerm f, Pretty v) => Pretty (Rule f v) where
   pPrint (Rule l r) =
     hang (pPrint l <+> text "->") 2 (pPrint r)
 
-pPrintStyle :: Pretty a => TermStyle -> PrettyLevel -> Rational -> Doc -> [a] -> Doc
-pPrintStyle Invisible _ _ d [] = d
-pPrintStyle Invisible l p _ [t] = pPrintPrec l p t
-pPrintStyle Invisible l p _ (t:ts) =
-  pPrintParen (p > 10) $
-    hang (pPrint t) 2
-      (fsep (map (pPrintPrec l 11) ts))
-pPrintStyle Curried _ _ d [] = d
-pPrintStyle Curried l p d xs =
-  pPrintParen (p > 10) $
-    hang d 2
-      (fsep (map (pPrintPrec l 11) xs))
-pPrintStyle Uncurried _ _ d [] = d
-pPrintStyle Uncurried l _ d xs =
-  d <> parens (fsep (punctuate comma (map (pPrintPrec l 0) xs)))
-pPrintStyle (Tuple arity) l p _ xs
-  | length xs >= arity =
-    pPrintStyle Curried l p
-      (pPrintTuple (take arity (map (pPrintPrec l 0) xs)))
-      (drop arity xs)
-  | otherwise =
-    pPrintStyle Curried l p
-      (text ("(" ++ replicate (arity-1) ',' ++ ")")) xs
-pPrintStyle Postfix l _ d [x] =
-  pPrintPrec l 11 x <> d
-pPrintStyle Postfix l p d xs =
-  pPrintStyle Curried l p (parens d) xs
-pPrintStyle Prefix l _ d [x] =
-  d <> pPrintPrec l 11 x
-pPrintStyle Prefix l p d xs =
-  pPrintStyle Curried l p (parens d) xs
-pPrintStyle TupleType l p d xs =
-  pPrintStyle (Tuple (length xs)) l p d xs
-pPrintStyle ListType l _ _ [x] =
-  brackets (pPrintPrec l 0 x)
-pPrintStyle ListType l p d xs =
-  pPrintStyle Curried l p d xs
-pPrintStyle Gyrator l _ d [x, y] =
-  d <> brackets (sep (punctuate comma [pPrintPrec l 0 x, pPrintPrec l 0 y]))
-pPrintStyle Gyrator l p d (x:y:zs) =
-  pPrintStyle Curried l p (pPrintStyle Gyrator l p d [x, y]) zs
-pPrintStyle Gyrator l p d xs = pPrintStyle Curried l p d xs
-pPrintStyle style l p d xs =
-  case xs of
-    [x, y] ->
-      pPrintParen (p > pOp) $
-        hang (pPrintPrec l (pOp+1) x <+> d) 2
-             (pPrintPrec l (pOp+r) y)
-    (x:y:xs) ->
-      pPrintParen (p > pOp) $
-        hang (pPrintStyle style l 11 d [x, y]) 2
-          (fsep (map (pPrintPrec l 11) xs))
-    [x] ->
-      parens (pPrintPrec l (pOp+1) x <+> d)
-    _ ->
-      pPrintParen (p > pOp) $
-        hang (parens d) 2
-          (fsep (map (pPrintPrec l 11) xs))
-  where
-    (r, pOp) =
-      case style of
-        Infixr pOp -> (0, fromIntegral pOp)
-        Infix  pOp -> (1, fromIntegral pOp)
-        _ -> ERROR("unknown style")
-
+-- | Generate a list of candidate names for pretty-printing.
 supply :: [String] -> [String]
 supply names =
   names ++
   [ name ++ show i | i <- [2..], name <- names ]
+
+-- * Pretty-printing of terms.
+
+instance (PrettyTerm f, Pretty v) => Pretty (Term f v) where
+  pPrintPrec l p (Var x) = pPrintPrec l p x
+  pPrintPrec l p (Fun f xs) = pPrintTerm (termStyle f) l p (pPrint f) xs
+
+-- | A class for customising the printing of function symbols.
+class Pretty a => PrettyTerm a where
+  termStyle :: a -> TermStyle
+  termStyle _ = curried
+
+-- | Defines how to print out a function symbol.
+newtype TermStyle =
+  TermStyle {
+    -- | Takes the pretty-printing level, precedence,
+    -- pretty-printed function symbol and list of arguments and prints the term.
+    pPrintTerm :: forall a. Pretty a => PrettyLevel -> Rational -> Doc -> [a] -> Doc }
+
+invisible, curried, uncurried, prefix, postfix :: TermStyle
+
+-- | For operators like @$@ that should be printed as a blank space.
+invisible =
+  TermStyle $ \l p d ->
+    let
+      f [] = d
+      f [t] = pPrintPrec l p t
+      f (t:ts) =
+        pPrintParen (p > 10) $
+          hang (pPrint t) 2
+            (fsep (map (pPrintPrec l 11) ts))
+    in f
+
+-- | For functions that should be printed curried.
+curried =
+  TermStyle $ \l p d ->
+    let
+      f [] = d
+      f xs =
+        pPrintParen (p > 10) $
+          hang d 2
+            (fsep (map (pPrintPrec l 11) xs))
+    in f
+
+-- | For functions that should be printed uncurried.
+uncurried =
+  TermStyle $ \l _ d ->
+    let
+      f [] = d
+      f xs =
+        d <> parens (fsep (punctuate comma (map (pPrintPrec l 0) xs)))
+    in f
+
+-- | A helper function that deals with under- and oversaturated applications.
+fixedArity :: Int -> TermStyle -> TermStyle
+fixedArity arity style =
+  TermStyle $ \l p d ->
+    let
+      f xs
+        | length xs < arity = pPrintTerm curried l p (parens d) xs
+        | length xs > arity =
+            pPrintParen (p > 10) $
+              fsep (parens (pPrintTerm style l 0 d ys):
+                    map (nest 2 . pPrintPrec l 11) zs)
+        | otherwise = pPrintTerm style l p d xs
+        where
+          (ys, zs) = splitAt arity xs
+    in f
+
+-- | For prefix operators.
+prefix =
+  fixedArity 1 $
+  TermStyle $ \l _ d [x] ->
+    d <> pPrintPrec l 11 x
+
+-- | For postfix operators.
+postfix =
+  fixedArity 1 $
+  TermStyle $ \l _ d [x] ->
+    pPrintPrec l 11 x <> d
+
+-- | For infix operators.
+-- e.g. @infixStyle (Left 5)@ corresponds to @infixl 5@.
+-- Currently terms that mix left- and right-associative
+-- operators are not printed correctly.
+infixStyle :: Either Int Int -> TermStyle
+infixStyle fixity =
+  fixedArity 2 $
+  TermStyle $ \l p d [x, y] ->
+    pPrintParen (p > pOp) $
+      hang (pPrintPrec l (pOp+1) x <+> d) 2
+           (pPrintPrec l (pOp+r) y)
+  where
+    (r, pOp) =
+      case fixity of
+        Left pOp -> (0, fromIntegral pOp)
+        Right pOp -> (1, fromIntegral pOp)
