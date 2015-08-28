b63b1bc7c710a0d4efa661fbc16ebc4853605596
Nick Smallbone <nick.smallbone@gmail.com>
2015-08-29 00:31:33 +0200

    Move scary array manipulation into its own module.

--- src/KBC/Term.hs
+++ src/KBC/Term.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE BangPatterns, CPP, PatternGuards, PatternSynonyms, ViewPatterns, RecordWildCards, GeneralizedNewtypeDeriving, RankNTypes, MagicHash, UnboxedTuples #-}
-module KBC.Term where
+module KBC.Term(module KBC.Term.Core, module KBC.Term) where
 
 #include "errors.h"
 import Prelude hiding (lookup)
+import KBC.Term.Core
 import Data.Primitive
 import Control.Monad
 import Control.Monad.ST.Strict
@@ -14,119 +15,13 @@ import GHC.Types(Int(..))
 import GHC.Prim
 import GHC.ST hiding (liftST)
 
-newtype Fun f = MkFun Int deriving (Eq, Ord)
-newtype Var   = MkVar Int deriving (Eq, Ord, Enum)
-
-data Symbol =
-  Symbol {
-    isFun :: Bool,
-    index :: Int,
-    size  :: Int }
-instance Show Symbol where
-  show Symbol{..}
-    | isFun = "f" ++ show index ++ "=" ++ show size
-    | otherwise = "x" ++ show index
-
-{-# INLINE toSymbol #-}
-toSymbol :: Int64 -> Symbol
-toSymbol n =
-  Symbol (n < 0)
-    (if n < 0 then
-       complement (fromIntegral (n `unsafeShiftR` 32))
-     else
-       fromIntegral (n `unsafeShiftR` 32))
-    (fromIntegral n .&. 0xffffffff)
-
-{-# INLINE fromSymbol #-}
-fromSymbol :: Symbol -> Int64
-fromSymbol Symbol{..}
-  | isFun =
-    fromIntegral (complement index) `unsafeShiftL` 32 +
-    fromIntegral size
-  | otherwise =
-    fromIntegral index `unsafeShiftL` 32 +
-    fromIntegral size
-
-data Term f =
-  Term {
-    root     :: {-# UNPACK #-} !Int64,
-    children :: {-# UNPACK #-} !(TermList f) }
-  deriving Eq
-
 instance Show (Term f) where
   show = show . singleton
-
-data TermList f =
-  TermList {
-    low   :: {-# UNPACK #-} !Int,
-    high  :: {-# UNPACK #-} !Int,
-    array :: {-# UNPACK #-} !ByteArray }
 
 instance Show (TermList f) where
   show (TermList lo hi arr) =
     "<" ++ show lo ++ "," ++ show hi ++ ">[" ++
       intercalate "," [show (toSymbol (indexByteArray arr n)) | n <- [lo..hi-1]] ++ "]"
-
-{-# INLINE nsymbols #-}
-nsymbols :: TermList f -> Int
-nsymbols (TermList low high _) = high - low
-
-instance Eq (TermList f) where
-  {-# INLINE (==) #-}
-  t == u = nsymbols t == nsymbols u && eqSameLength t u
-
-eqSameLength :: TermList f -> TermList f -> Bool
-eqSameLength Empty !_ = True
-eqSameLength (ConsSym s1 t) (UnsafeConsSym s2 u) =
-  root s1 == root s2 && eqSameLength t u
-
-{-# INLINE unsafePatHead #-}
-unsafePatHead :: TermList f -> Maybe (Term f, TermList f, TermList f)
-unsafePatHead TermList{..} =
-  Just (Term x (TermList low (low+size) array),
-        TermList (low+1) high array,
-        TermList (low+size) high array)
-  where
-    x = indexByteArray array low
-    Symbol{..} = toSymbol x
-
-{-# INLINE patHead #-}
-patHead :: TermList f -> Maybe (Term f, TermList f, TermList f)
-patHead t@TermList{..}
-  | low == high = Nothing
-  | otherwise = unsafePatHead t
-
-pattern Empty <- (patHead -> Nothing)
-pattern Cons t ts <- (patHead -> Just (t, _, ts))
-pattern ConsSym t ts <- (patHead -> Just (t, ts, _))
-pattern UnsafeCons t ts <- (unsafePatHead -> Just (t, _, ts))
-pattern UnsafeConsSym t ts <- (unsafePatHead -> Just (t, ts, _))
-
-{-# INLINE patRoot #-}
-patRoot :: Int64 -> Either Var (Fun f)
-patRoot root
-  | isFun     = Right (MkFun index)
-  | otherwise = Left (MkVar index)
-  where
-    Symbol{..} = toSymbol root
-
-{-# INLINE patNext #-}
-patNext :: TermList f -> TermList f
-patNext (TermList lo hi array) = TermList (lo+1) hi array
-
-pattern Var x <- Term (patRoot -> Left x) _
-pattern Fun f ts <- Term (patRoot -> Right f) (patNext -> ts)
-
-{-# INLINE singleton #-}
-singleton :: Term f -> TermList f
-singleton Term{children = TermList{..}} =
-  TermList low high array
-
-data Subst f =
-  Subst {
-    term  :: {-# UNPACK #-} !ByteArray,
-    vars  :: {-# UNPACK #-} !Int,
-    subst :: {-# UNPACK #-} !ByteArray }
 
 toSubst :: Subst f -> [(Int, Term f)]
 toSubst subst@(Subst _ n _) =
@@ -137,66 +32,11 @@ toSubst subst@(Subst _ n _) =
 instance Show (Subst f) where
   show = show . toSubst
 
-data MSubst s f =
-  MSubst {
-    mterm  :: {-# UNPACK #-} !ByteArray,
-    mvars  :: {-# UNPACK #-} !Int,
-    msubst :: {-# UNPACK #-} !(MutableByteArray s) }
-
-{-# INLINE toRange #-}
-toRange :: Word64 -> Maybe (Int, Int)
-toRange 0 = Nothing
-toRange n = Just (fromIntegral n .&. 0xffffffff, fromIntegral (n `unsafeShiftR` 32))
-
-{-# INLINE fromRange #-}
-fromRange :: (Int, Int) -> Word64
-fromRange (lo, hi) =
-  fromIntegral lo .&. 0xffffffff +
-  fromIntegral hi `unsafeShiftL` 32
-
 {-# INLINE lookup #-}
 lookup :: Subst f -> Var -> Maybe (Term f)
 lookup s x = do
   Cons t Empty <- lookupList s x
   return t
-
-{-# INLINE lookupList #-}
-lookupList :: Subst f -> Var -> Maybe (TermList f)
-lookupList Subst{..} (MkVar x)
-  | x >= vars = Nothing
-  | otherwise = do
-    (lo, hi) <- toRange (indexByteArray subst x)
-    return (TermList lo hi term)
-
--- Note to self: have KBC.Term.Unsafe which exports raw versions of functions,
--- KBC.Term which exports checked versions.
-{-# INLINE extend #-}
-extend :: MSubst s f -> Var -> Term f -> ST s (Maybe ())
-extend MSubst{..} (MkVar x) t = do
-  let TermList lo hi _ = singleton t
-  rng <- fmap toRange (readByteArray msubst x)
-  case rng of
-    Nothing -> do
-      writeByteArray msubst x (fromRange (lo, hi))
-      return (Just ())
-    Just (lo', hi')
-      | TermList lo hi mterm == TermList lo' hi' mterm ->
-        return (Just ())
-      | otherwise ->
-        return Nothing
-
-{-# INLINE freezeSubst #-}
-freezeSubst :: MSubst s f -> ST s (Subst f)
-freezeSubst MSubst{..} = do
-  subst <- unsafeFreezeByteArray msubst
-  return (Subst mterm mvars subst)
-
-{-# INLINE newMSubst #-}
-newMSubst :: TermList f -> Int -> ST s (MSubst s f)
-newMSubst TermList{..} vars = do
-  subst <- newByteArray (vars * sizeOf (fromRange __))
-  setByteArray subst 0 vars (0 `asTypeOf` fromRange __)
-  return (MSubst array vars subst)
 
 {-# INLINE bound #-}
 bound :: TermList f -> Int
@@ -212,14 +52,19 @@ bound t = aux 0 t
 match :: Term f -> Term f -> Maybe (Subst f)
 match pat t = matchTerms (singleton pat) (singleton t)
 
+{-# INLINE emitTerm #-}
+emitTerm :: Term f -> BuildM s f ()
+emitTerm t = emitTermList (singleton t)
+
 matchTerms :: TermList f -> TermList f -> Maybe (Subst f)
 matchTerms !pat !t = runST $ do
-  subst <- newMSubst t (bound pat)
+  subst <- newMutableSubst t (bound pat)
   let loop !_ !_ | False = __
       loop Empty _ = fmap Just (freezeSubst subst)
       loop _ Empty = __
       loop (ConsSym (Fun f _) pat) (ConsSym (Fun g _) t)
         | f == g = loop pat t
+      -- XXX change to Cons?
       loop (ConsSym (Var x) pat) (Cons t u) = do
         res <- extend subst x t
         case res of
@@ -227,100 +72,6 @@ matchTerms !pat !t = runST $ do
           Just () -> loop pat u
       loop _ _ = return Nothing
   loop pat t
-
-newtype BuildTermListM s f a =
-  BuildM {
-    unBuildM ::
-      MutableByteArray# s -> Int# -> State# s -> (# State# s, Int#, a #) }
-
-instance Functor (BuildTermListM s f) where
-  {-# INLINE fmap #-}
-  fmap f (BuildM m) =
-    BuildM $ \array i s ->
-      case m array i s of
-        (# s, j, x #) -> (# s, j, f x #)
-instance Applicative (BuildTermListM s f) where
-  pure = return
-  (<*>) = ap
-instance Monad (BuildTermListM s f) where
-  {-# INLINE return #-}
-  return x = BuildM (\_ i s -> (# s, i, x #))
-  {-# INLINE (>>=) #-}
-  BuildM m >>= f =
-    BuildM $ \array i s ->
-      case m array i s of
-        (# s, j, x #) -> unBuildM (f x) array j s
-
-{-# INLINE getArray #-}
-getArray :: BuildTermListM s f (MutableByteArray s)
-getArray = BuildM $ \array n s -> (# s, n, MutableByteArray array #)
-
-{-# INLINE getIndex #-}
-getIndex :: BuildTermListM s f Int
-getIndex = BuildM $ \_ n s -> (# s, n, I# n #)
-
-{-# INLINE putIndex #-}
-putIndex :: Int -> BuildTermListM s f ()
-putIndex (I# n) = BuildM $ \_ _ s -> (# s, n, () #)
-
-{-# INLINE liftST #-}
-liftST :: ST s a -> BuildTermListM s f a
-liftST (ST m) =
-  BuildM $ \_ n s ->
-  case m s of
-    (# s, x #) -> (# s, n, x #)
-
-{-# INLINE freezeTermList #-}
-freezeTermList :: BuildTermListM s f (TermList f)
-freezeTermList = do
-  marray <- getArray
-  n <- getIndex
-  !array <- liftST $ unsafeFreezeByteArray marray
-  return (TermList 0 n array)
-
-{-# INLINE buildTermList #-}
-buildTermList :: Int -> (forall s. BuildTermListM s f a) -> a
-buildTermList size m =
-  runST $ do
-    MutableByteArray array <- newByteArray (size * sizeOf (fromSymbol __))
-    ST $ \s ->
-      case unBuildM m array 0# s of
-        (# s, _, x #) -> (# s, x #)
-
-{-# INLINE appendPrim #-}
-appendPrim :: Symbol -> BuildTermListM s f () -> BuildTermListM s f ()
-appendPrim x inner = do
-  array <- getArray
-  n <- getIndex
-  putIndex (n+1)
-  inner
-  m <- getIndex
-  liftST $ writeByteArray array n (fromSymbol x { size = m - n })
-
-{-# INLINE appendRoot #-}
-appendRoot :: Term f -> BuildTermListM s f () -> BuildTermListM s f ()
-appendRoot t inner = appendPrim (toSymbol (root t)) inner
-
-{-# INLINE appendFun #-}
-appendFun :: Fun f -> BuildTermListM s f () -> BuildTermListM s f ()
-appendFun (MkFun f) inner = appendPrim (Symbol True f 0) inner
-
-{-# INLINE appendVar #-}
-appendVar :: Var -> BuildTermListM s f ()
-appendVar (MkVar x) = appendPrim (Symbol False x 1) (return ())
-
-{-# INLINE appendTermList #-}
-appendTermList :: TermList f -> BuildTermListM s f ()
-appendTermList (TermList lo hi array) = do
-  marray <- getArray
-  n <- getIndex
-  let k = sizeOf (fromSymbol __)
-  liftST $ copyByteArray marray (n*k) array (lo*k) ((hi-lo)*k)
-  putIndex (n + hi-lo)
-
-{-# INLINE appendTerm #-}
-appendTerm :: Term f -> BuildTermListM s f ()
-appendTerm t = appendTermList (singleton t)
 
 data CompoundTerm f =
     CFlat  (Term f)
@@ -335,28 +86,28 @@ flattenTerm t =
 
 flattenTerms :: [CompoundTerm f] -> TermList f
 flattenTerms ts =
-  buildTermList (sum (map len ts)) $ do
+  buildTermList (sum (map clen ts)) $ do
     let
       loop [] = return ()
       loop (CFlat t:ts) = do
-        appendTerm t
+        emitTerm t
         loop ts
       loop (CFun f ts:us) = do
-        appendFun f (loop ts)
+        emitFun f (loop ts)
         loop us
       loop (CVar x:ts) = do
-        appendVar x
+        emitVar x
         loop ts
       loop (CSubst sub t:ts) = do
-        appendSubst sub t
+        emitSubst sub t
         loop ts
     loop ts
     freezeTermList
   where
-    len (CFlat t) = nsymbols (singleton t)
-    len (CFun _ ts) = 1 + sum (map len ts)
-    len (CVar _) = 1
-    len (CSubst sub t) = substSize sub (singleton t)
+    clen (CFlat t) = len (singleton t)
+    clen (CFun _ ts) = 1 + sum (map clen ts)
+    clen (CVar _) = 1
+    clen (CSubst sub t) = substSize sub (singleton t)
 
 {-# INLINE subst_ #-}
 subst_ :: Subst f -> Term f -> Term f
@@ -367,7 +118,7 @@ subst_ sub t =
 substList :: Subst f -> TermList f -> TermList f
 substList sub t =
   buildTermList (substSize sub t) $ do
-    appendSubstList sub t
+    emitSubstList sub t
     freezeTermList
 
 {-# INLINE substSize #-}
@@ -379,22 +130,22 @@ substSize !sub = aux 0
     aux n (ConsSym (Var x) t) =
       case lookupList sub x of
         Nothing -> aux (n+1) t
-        Just u  -> aux (n+nsymbols u) t
+        Just u  -> aux (n+len u) t
 
-{-# INLINE appendSubst #-}
-appendSubst :: Subst f -> Term f -> BuildTermListM s f ()
-appendSubst sub t = appendSubstList sub (singleton t)
+{-# INLINE emitSubst #-}
+emitSubst :: Subst f -> Term f -> BuildM s f ()
+emitSubst sub t = emitSubstList sub (singleton t)
 
-{-# INLINE appendSubstList #-}
-appendSubstList :: Subst f -> TermList f -> BuildTermListM s f ()
-appendSubstList !sub = aux
+{-# INLINE emitSubstList #-}
+emitSubstList :: Subst f -> TermList f -> BuildM s f ()
+emitSubstList !sub = aux
   where
     aux Empty = return ()
     aux (Cons v@(Var x) ts) = do
       case lookupList sub x of
-        Nothing -> appendVar x
-        Just u  -> appendTermList u
+        Nothing -> emitVar x
+        Just u  -> emitTermList u
       aux ts
     aux (Cons x@(Fun f ts) us) = do
-      appendFun f (aux ts)
+      emitFun f (aux ts)
       aux us
---
+++ src/KBC/Term/Core.hs
@@ -0,0 +1,343 @@
+-- Terms and substitutions, implemented using flatterms.
+-- This module contains all the low-level representation stuff.
+{-# LANGUAGE BangPatterns, CPP, PatternGuards, PatternSynonyms, ViewPatterns, RecordWildCards, GeneralizedNewtypeDeriving, RankNTypes, MagicHash, UnboxedTuples #-}
+module KBC.Term.Core where
+
+#include "errors.h"
+import Data.Primitive
+import Control.Monad
+import Control.Monad.ST.Strict
+import Data.Bits
+import Data.Int
+import Data.Word
+import Data.List hiding (lookup)
+import GHC.Types(Int(..))
+import GHC.Prim
+import GHC.ST hiding (liftST)
+
+--------------------------------------------------------------------------------
+-- Symbols. A symbol is a single function or variable in a flatterm.
+--------------------------------------------------------------------------------
+
+data Symbol =
+  Symbol {
+    -- Is it a function?
+    isFun :: Bool,
+    -- What is its number?
+    index :: Int,
+    -- What is the size of the term rooted at this symbol?
+    size  :: Int }
+
+instance Show Symbol where
+  show Symbol{..}
+    | isFun = "f" ++ show index ++ "=" ++ show size
+    | otherwise = "x" ++ show index
+
+-- Convert symbols to/from Int64 for storage in flatterms.
+-- The encoding:
+--   * bits 0-31:  size
+--   * bits 32-63: index (variable) or ~index (function)
+{-# INLINE toSymbol #-}
+toSymbol :: Int64 -> Symbol
+toSymbol n =
+  Symbol (n < 0)
+    (if n < 0 then
+       complement (fromIntegral (n `unsafeShiftR` 32))
+     else
+       fromIntegral (n `unsafeShiftR` 32))
+    (fromIntegral n .&. 0xffffffff)
+
+{-# INLINE fromSymbol #-}
+fromSymbol :: Symbol -> Int64
+fromSymbol Symbol{..}
+  | isFun =
+    fromIntegral (complement index) `unsafeShiftL` 32 +
+    fromIntegral size
+  | otherwise =
+    fromIntegral index `unsafeShiftL` 32 +
+    fromIntegral size
+
+--------------------------------------------------------------------------------
+-- Flatterms, or rather lists of terms.
+--------------------------------------------------------------------------------
+
+-- A TermList is a slice of an unboxed array of symbols.
+data TermList f =
+  TermList {
+    low   :: {-# UNPACK #-} !Int,
+    high  :: {-# UNPACK #-} !Int,
+    array :: {-# UNPACK #-} !ByteArray }
+
+{-# INLINE len #-}
+-- The length (number of symbols in) a flatterm.
+len :: TermList f -> Int
+len (TermList low high _) = high - low
+
+-- A term is a special case of a termlist.
+-- We store it as the termlist together with the root symbol.
+data Term f =
+  Term {
+    root     :: {-# UNPACK #-} !Int64,
+    termlist :: {-# UNPACK #-} !(TermList f) }
+
+instance Eq (Term f) where
+  x == y = termlist x == termlist y
+
+-- Pattern synonyms for termlists:
+-- * Empty :: TermList f
+--   Empty is the empty termlist.
+-- * Cons t ts :: Term f -> TermList f -> TermList f
+--   Cons t ts is the termlist t:ts.
+-- * ConsSym t ts :: Term f -> TermList f -> TermList f
+--   ConsSym t ts is like Cons t ts but ts also includes t's children
+--   (operationally, ts seeks one term to the right in the termlist).
+-- * UnsafeCons/UnsafeConsSym: like Cons and ConsSym but don't check
+--   that the termlist is non-empty.
+pattern Empty <- (patHead -> Nothing)
+pattern Cons t ts <- (patHead -> Just (t, _, ts))
+pattern ConsSym t ts <- (patHead -> Just (t, ts, _))
+pattern UnsafeCons t ts <- (unsafePatHead -> Just (t, _, ts))
+pattern UnsafeConsSym t ts <- (unsafePatHead -> Just (t, ts, _))
+
+{-# INLINE unsafePatHead #-}
+unsafePatHead :: TermList f -> Maybe (Term f, TermList f, TermList f)
+unsafePatHead TermList{..} =
+  Just (Term x (TermList low (low+size) array),
+        TermList (low+1) high array,
+        TermList (low+size) high array)
+  where
+    x = indexByteArray array low
+    Symbol{..} = toSymbol x
+
+{-# INLINE patHead #-}
+patHead :: TermList f -> Maybe (Term f, TermList f, TermList f)
+patHead t@TermList{..}
+  | low == high = Nothing
+  | otherwise = unsafePatHead t
+
+-- Pattern synonyms for single terms.
+-- * Var :: Var -> Term f
+-- * Fun :: Fun f -> TermList f -> Term f
+newtype Fun f = MkFun Int deriving (Eq, Ord)
+newtype Var   = MkVar Int deriving (Eq, Ord, Enum)
+
+pattern Var x <- Term (patRoot -> Left x) _
+pattern Fun f ts <- Term (patRoot -> Right f) (patNext -> ts)
+
+{-# INLINE patRoot #-}
+patRoot :: Int64 -> Either Var (Fun f)
+patRoot root
+  | isFun     = Right (MkFun index)
+  | otherwise = Left (MkVar index)
+  where
+    Symbol{..} = toSymbol root
+
+{-# INLINE patNext #-}
+patNext :: TermList f -> TermList f
+patNext (TermList lo hi array) = TermList (lo+1) hi array
+
+-- Convert a term to a termlist.
+{-# INLINE singleton #-}
+singleton :: Term f -> TermList f
+singleton Term{..} = termlist
+
+-- We can implement equality almost without access to the
+-- internal representation of the termlists, but we cheat by
+-- comparing Int64s instead of Symbols.
+instance Eq (TermList f) where
+  {-# INLINE (==) #-}
+  t == u = len t == len u && eqSameLength t u
+
+eqSameLength :: TermList f -> TermList f -> Bool
+eqSameLength Empty !_ = True
+eqSameLength (ConsSym s1 t) (UnsafeConsSym s2 u) =
+  root s1 == root s2 && eqSameLength t u
+
+--------------------------------------------------------------------------------
+-- Building terms imperatively.
+--------------------------------------------------------------------------------
+
+-- A monad for building terms.
+newtype BuildM s f a =
+  BuildM {
+    unBuildM ::
+      -- Takes: the term array, and current position in the term.
+      -- Returns the final position.
+      MutableByteArray# s -> Int# -> State# s -> (# State# s, Int#, a #) }
+
+instance Functor (BuildM s f) where
+  {-# INLINE fmap #-}
+  fmap f (BuildM m) =
+    BuildM $ \array i s ->
+      case m array i s of
+        (# s, j, x #) -> (# s, j, f x #)
+
+instance Applicative (BuildM s f) where
+  pure = return
+  (<*>) = ap
+instance Monad (BuildM s f) where
+  {-# INLINE return #-}
+  return x = BuildM (\_ i s -> (# s, i, x #))
+  {-# INLINE (>>=) #-}
+  BuildM m >>= f =
+    BuildM $ \array i s ->
+      case m array i s of
+        (# s, j, x #) -> unBuildM (f x) array j s
+
+{-# INLINE getArray #-}
+getArray :: BuildM s f (MutableByteArray s)
+getArray = BuildM $ \array n s -> (# s, n, MutableByteArray array #)
+
+{-# INLINE getIndex #-}
+getIndex :: BuildM s f Int
+getIndex = BuildM $ \_ n s -> (# s, n, I# n #)
+
+{-# INLINE putIndex #-}
+putIndex :: Int -> BuildM s f ()
+putIndex (I# n) = BuildM $ \_ _ s -> (# s, n, () #)
+
+{-# INLINE liftST #-}
+liftST :: ST s a -> BuildM s f a
+liftST (ST m) =
+  BuildM $ \_ n s ->
+  case m s of
+    (# s, x #) -> (# s, n, x #)
+
+-- Construct a term, given its size and a builder for it.
+{-# INLINE buildTermList #-}
+buildTermList :: Int -> (forall s. BuildM s f a) -> a
+buildTermList size m =
+  runST $ do
+    MutableByteArray array <- newByteArray (size * sizeOf (fromSymbol __))
+    ST $ \s ->
+      case unBuildM m array 0# s of
+        (# s, _, x #) -> (# s, x #)
+
+-- Freeze a term that's been built.
+{-# INLINE freezeTermList #-}
+freezeTermList :: BuildM s f (TermList f)
+freezeTermList = do
+  marray <- getArray
+  n <- getIndex
+  !array <- liftST $ unsafeFreezeByteArray marray
+  return (TermList 0 n array)
+
+-- Emit a single symbol (for internal use mostly).
+{-# INLINE emitSymbol #-}
+emitSymbol :: Symbol -> BuildM s f () -> BuildM s f ()
+emitSymbol x inner = do
+  array <- getArray
+  n <- getIndex
+  putIndex (n+1)
+  inner
+  m <- getIndex
+  liftST $ writeByteArray array n (fromSymbol x { size = m - n })
+
+-- Emit the root of a term.
+-- The second argument is called to emit the children.
+{-# INLINE emitRoot #-}
+emitRoot :: Term f -> BuildM s f () -> BuildM s f ()
+emitRoot t inner = emitSymbol (toSymbol (root t)) inner
+
+-- Emit a function symbol.
+-- The second argument is called to emit the function's arguments.
+{-# INLINE emitFun #-}
+emitFun :: Fun f -> BuildM s f () -> BuildM s f ()
+emitFun (MkFun f) inner = emitSymbol (Symbol True f 0) inner
+
+-- Emit a variable.
+{-# INLINE emitVar #-}
+emitVar :: Var -> BuildM s f ()
+emitVar (MkVar x) = emitSymbol (Symbol False x 1) (return ())
+
+-- Emit a whole termlist.
+{-# INLINE emitTermList #-}
+emitTermList :: TermList f -> BuildM s f ()
+emitTermList (TermList lo hi array) = do
+  marray <- getArray
+  n <- getIndex
+  let k = sizeOf (fromSymbol __)
+  liftST $ copyByteArray marray (n*k) array (lo*k) ((hi-lo)*k)
+  putIndex (n + hi-lo)
+
+--------------------------------------------------------------------------------
+-- Substitutions.
+--------------------------------------------------------------------------------
+
+-- Substitutions are optimised for matching, where all of the bindings
+-- are subterms of a single term (the one we are matching against).
+-- A substitution consists of:
+-- * A term, the backing term. All of the bindings are subterms of this.
+-- * An array, mapping variable numbers to slices of the backing term
+--   (pairs of positions).
+data Subst f =
+  Subst {
+    -- The backing term.
+    term  :: {-# UNPACK #-} !ByteArray,
+    -- The number of variables in the bindings array.
+    vars  :: {-# UNPACK #-} !Int,
+    -- The bindings: an unboxed array of (Int, Int) pairs.
+    subst :: {-# UNPACK #-} !ByteArray }
+
+-- Convert between slices and Word64s for storage.
+{-# INLINE toSlice #-}
+toSlice :: Word64 -> Maybe (Int, Int)
+toSlice 0 = Nothing
+toSlice n = Just (fromIntegral n .&. 0xffffffff, fromIntegral (n `unsafeShiftR` 32))
+
+{-# INLINE fromSlice #-}
+fromSlice :: (Int, Int) -> Word64
+fromSlice (lo, hi) =
+  fromIntegral lo .&. 0xffffffff +
+  fromIntegral hi `unsafeShiftL` 32
+
+-- Look up a variable.
+{-# INLINE lookupList #-}
+lookupList :: Subst f -> Var -> Maybe (TermList f)
+lookupList Subst{..} (MkVar x)
+  | x >= vars = Nothing
+  | otherwise = do
+    (lo, hi) <- toSlice (indexByteArray subst x)
+    return (TermList lo hi term)
+
+-- Mutable substitutions, used for building substitutions.
+data MutableSubst s f =
+  MutableSubst {
+    mterm  :: {-# UNPACK #-} !ByteArray,
+    mvars  :: {-# UNPACK #-} !Int,
+    msubst :: {-# UNPACK #-} !(MutableByteArray s) }
+
+-- Create an empty substitution, given the backing term
+-- and the number of variables required.
+{-# INLINE newMutableSubst #-}
+newMutableSubst :: TermList f -> Int -> ST s (MutableSubst s f)
+newMutableSubst TermList{..} vars = do
+  subst <- newByteArray (vars * sizeOf (fromSlice __))
+  setByteArray subst 0 vars (0 `asTypeOf` fromSlice __)
+  return (MutableSubst array vars subst)
+
+-- Freeze a mutable substitution.
+{-# INLINE freezeSubst #-}
+freezeSubst :: MutableSubst s f -> ST s (Subst f)
+freezeSubst MutableSubst{..} = do
+  subst <- unsafeFreezeByteArray msubst
+  return (Subst mterm mvars subst)
+
+-- Add a new binding to a substitution.
+-- Returns Just () on success or Nothing if an incompatible
+-- binding already existed.
+{-# INLINE extend #-}
+extend :: MutableSubst s f -> Var -> Term f -> ST s (Maybe ())
+extend _ x (Var y) | x == y = return (Just ())
+extend MutableSubst{..} (MkVar x) t = do
+  let TermList lo hi _ = singleton t
+  rng <- fmap toSlice (readByteArray msubst x)
+  case rng of
+    Nothing -> do
+      writeByteArray msubst x (fromSlice (lo, hi))
+      return (Just ())
+    Just (lo', hi')
+      | TermList lo hi mterm == TermList lo' hi' mterm ->
+        return (Just ())
+      | otherwise ->
+        return Nothing
