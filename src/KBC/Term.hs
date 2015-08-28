23b1a946c3eb3a3bb8ce8d4bf189dd2deb3a1440
Nick Smallbone <nick.smallbone@gmail.com>
2015-08-29 01:05:52 +0200

    Rearrange stuff a bit.

--- src/KBC/Term.hs
+++ src/KBC/Term.hs
@@ -23,28 +23,48 @@ import Control.Monad.ST.Strict
 import Data.List hiding (lookup)
 
 --------------------------------------------------------------------------------
--- Matching.
+-- A helper datatype for building terms.
 --------------------------------------------------------------------------------
 
-{-# INLINE match #-}
-match :: Term f -> Term f -> Maybe (Subst f)
-match pat t = matchList (singleton pat) (singleton t)
+-- An algebraic data type for terms, with flatterms or substitutions applied
+-- to flatterms at the leaf.
+data CompoundTerm f =
+    CFlat  (Term f)
+  | CFun   (Fun f) [CompoundTerm f]
+  | CVar   Var
+  | CSubst (Subst f) (Term f)
 
-matchList :: TermList f -> TermList f -> Maybe (Subst f)
-matchList !pat !t = runST $ do
-  subst <- newMutableSubst t (bound pat)
-  let loop !_ !_ | False = __
-      loop Empty _ = fmap Just (freezeSubst subst)
-      loop _ Empty = __
-      loop (ConsSym (Fun f _) pat) (ConsSym (Fun g _) t)
-        | f == g = loop pat t
-      loop (Cons (Var x) pat) (Cons t u) = do
-        res <- extend subst x t
-        case res of
-          Nothing -> return Nothing
-          Just () -> loop pat u
-      loop _ _ = return Nothing
-  loop pat t
+-- Turn a compound term into a flatterm.
+flattenTerm :: CompoundTerm f -> Term f
+flattenTerm t =
+  case flattenList [t] of
+    Cons u Empty -> u
+
+-- Turn a list of compound term into a list of flatterms.
+flattenList :: [CompoundTerm f] -> TermList f
+flattenList ts =
+  buildTermList (sum (map clen ts)) $ do
+    let
+      loop [] = return ()
+      loop (CFlat t:ts) = do
+        emitTerm t
+        loop ts
+      loop (CFun f ts:us) = do
+        emitFun f (loop ts)
+        loop us
+      loop (CVar x:ts) = do
+        emitVar x
+        loop ts
+      loop (CSubst sub t:ts) = do
+        emitSubst sub t
+        loop ts
+    loop ts
+    freezeTermList
+  where
+    clen (CFlat t) = len (singleton t)
+    clen (CFun _ ts) = 1 + sum (map clen ts)
+    clen (CVar _) = 1
+    clen (CSubst sub t) = substTermSize sub (singleton t)
 
 --------------------------------------------------------------------------------
 -- Substitution.
@@ -95,48 +115,28 @@ emitSubstList !sub = aux
       aux us
 
 --------------------------------------------------------------------------------
--- A helper datatype for building terms.
+-- Matching.
 --------------------------------------------------------------------------------
 
--- An algebraic data type for terms, with flatterms or substitutions applied
--- to flatterms at the leaf.
-data CompoundTerm f =
-    CFlat  (Term f)
-  | CFun   (Fun f) [CompoundTerm f]
-  | CVar   Var
-  | CSubst (Subst f) (Term f)
+{-# INLINE match #-}
+match :: Term f -> Term f -> Maybe (Subst f)
+match pat t = matchList (singleton pat) (singleton t)
 
--- Turn a compound term into a flatterm.
-flattenTerm :: CompoundTerm f -> Term f
-flattenTerm t =
-  case flattenList [t] of
-    Cons u Empty -> u
-
--- Turn a list of compound term into a list of flatterms.
-flattenList :: [CompoundTerm f] -> TermList f
-flattenList ts =
-  buildTermList (sum (map clen ts)) $ do
-    let
-      loop [] = return ()
-      loop (CFlat t:ts) = do
-        emitTerm t
-        loop ts
-      loop (CFun f ts:us) = do
-        emitFun f (loop ts)
-        loop us
-      loop (CVar x:ts) = do
-        emitVar x
-        loop ts
-      loop (CSubst sub t:ts) = do
-        emitSubst sub t
-        loop ts
-    loop ts
-    freezeTermList
-  where
-    clen (CFlat t) = len (singleton t)
-    clen (CFun _ ts) = 1 + sum (map clen ts)
-    clen (CVar _) = 1
-    clen (CSubst sub t) = substTermSize sub (singleton t)
+matchList :: TermList f -> TermList f -> Maybe (Subst f)
+matchList !pat !t = runST $ do
+  subst <- newMutableSubst t (bound pat)
+  let loop !_ !_ | False = __
+      loop Empty _ = fmap Just (freezeSubst subst)
+      loop _ Empty = __
+      loop (ConsSym (Fun f _) pat) (ConsSym (Fun g _) t)
+        | f == g = loop pat t
+      loop (Cons (Var x) pat) (Cons t u) = do
+        res <- extend subst x t
+        case res of
+          Nothing -> return Nothing
+          Just () -> loop pat u
+      loop _ _ = return Nothing
+  loop pat t
 
 --------------------------------------------------------------------------------
 -- Miscellaneous stuff.
