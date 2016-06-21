03a64621e6aeb1e2eb4aaaa2194c73cec6aa2c22
Nick Smallbone <nick.smallbone@gmail.com>
2016-06-21 15:00:20 +0200

    Twiddle with index

--- src/Twee/Index.hs
+++ src/Twee/Index.hs
@@ -8,7 +8,7 @@ module Twee.Index where
 #include "errors.h"
 import qualified Prelude
 import Prelude hiding (filter, map, null)
-import Twee.Base hiding (var, fun, empty, vars, size)
+import Twee.Base hiding (var, fun, empty, vars, size, singleton, prefix)
 import qualified Twee.Term as Term
 import Twee.Array
 import qualified Data.List as List
@@ -17,13 +17,11 @@ import Twee.Profile
 
 data Index a =
   Index {
-    size :: {-# UNPACK #-} !Int,
-    here :: [Entry a],
-    fun  :: {-# UNPACK #-} !(Array (Index a)),
-    var  :: !(Index a) } |
-  Singleton {
-    key   :: {-# UNPACK #-} !(TermListOf a),
-    value :: {-# UNPACK #-} !(Entry a) } |
+    size   :: {-# UNPACK #-} !Int, -- size of smallest term, not including prefix
+    prefix :: {-# UNPACK #-} !(TermListOf a),
+    here   :: [Entry a],
+    fun    :: {-# UNPACK #-} !(Array (Index a)),
+    var    :: !(Index a) } |
   Nil
   deriving Show
 
@@ -42,17 +40,33 @@ null _ = False
 
 {-# INLINEABLE singleton #-}
 singleton :: Symbolic a => a -> Index a
-singleton x = Singleton (Term.singleton t) (Entry t x)
+singleton x = singletonEntry (Term.singleton t) (Entry t x)
   where
     t = term x
+
+{-# INLINE singletonEntry #-}
+singletonEntry :: TermListOf a -> Entry a -> Index a
+singletonEntry t e =
+  Index 0 t [e] newArray Nil
+
+{-# INLINE withPrefix #-}
+withPrefix :: (ConstantOf a ~ f) => TermList f -> Index a -> Index a
+withPrefix Empty idx = idx
+withPrefix _ Nil = Nil
+withPrefix t idx@Index{..} =
+  idx{prefix = buildList (builder t `mappend` builder prefix)}
 
 {-# INLINEABLE insert #-}
 insert :: Symbolic a => a -> Index a -> Index a
 insert x0 !idx = aux (Term.singleton t) idx
   where
-    aux t Nil = Singleton t x
-    aux t (Singleton u x) = aux t (expand u x)
-    aux Empty idx@Index{..} = idx { size = 0, here = x:here }
+    aux t Nil = singletonEntry t x
+    aux (Cons t ts) idx@Index{prefix = Cons u us} | t == u =
+      withPrefix (Term.singleton t) (aux ts idx{prefix = us})
+    aux t idx@Index{prefix = Cons{}} = aux t (expand idx)
+
+    aux Empty idx =
+      idx { size = 0, here = x:here idx }
     aux t@(ConsSym (Fun (MkFun f _) _) u) idx =
       idx {
         size = lenList t `min` size idx,
@@ -67,27 +81,28 @@ insert x0 !idx = aux (Term.singleton t) idx
     x  = Entry t x0
 
 {-# INLINE expand #-}
-expand :: TermListOf a -> Entry a -> Index a
-expand Empty x = Index 0 [x] newArray Nil
-expand (ConsSym s t) x =
-  Index (1+lenList t) [] fun var
-  where
-    (fun, var) =
-      case s of
-        Fun (MkFun f _) _ ->
-          (update f (Singleton t x) newArray, Nil)
-        Var _ ->
-          (newArray, Singleton t x)
+expand :: Index a -> Index a
+expand idx@Index{prefix = ConsSym t ts} =
+  case t of
+    Var _ ->
+      Index (size idx + 1 + lenList ts) emptyTermList [] newArray
+        idx { prefix = ts }
+    Fun (MkFun f _) us ->
+      Index (size idx + 1 + lenList ts) emptyTermList []
+        (update f idx { prefix = ts } newArray)
+        Nil
 
 {-# INLINEABLE delete #-}
 delete :: (Eq a, Symbolic a) => a -> Index a -> Index a
 delete x0 !idx = aux (Term.singleton t) idx
   where
     aux _ Nil = Nil
-    aux t idx@(Singleton u y)
-      | t == u && x == y = Nil
-      | otherwise        = idx
-    aux Empty idx = idx { here = List.delete x (here idx) }
+    aux (Cons t ts) idx@Index{prefix = Cons u us} | t == u =
+      withPrefix (Term.singleton t) (aux ts idx{prefix = us})
+    aux _ idx@Index{prefix = Cons{}} = idx
+
+    aux Empty idx =
+      idx { here = List.delete x (here idx) }
     aux (ConsSym (Fun (MkFun f _) _) t) idx =
       idx { fun = update f (aux t (fun idx ! f)) (fun idx) }
     aux (ConsSym (Var _) t) idx =
@@ -100,9 +115,10 @@ elem :: (Eq a, Symbolic a) => a -> Index a -> Bool
 elem x0 !idx = aux (Term.singleton t) idx
   where
     aux _ Nil = False
-    aux t (Singleton u y)
-      | t == u && x == y = True
-      | otherwise        = False
+    aux (Cons t ts) idx@Index{prefix = Cons u us} | t == u =
+      aux ts idx{prefix = us}
+    aux _ idx@Index{prefix = Cons{}} = False
+
     aux Empty idx = List.elem x (here idx)
     aux (ConsSym (Fun (MkFun f _) _) t) idx =
       aux t (fun idx ! f)
@@ -134,21 +150,27 @@ freeze idx = Frozen $ \t -> find t idx []
 find :: TermListOf a -> Index a -> [Match a] -> [Match a]
 find t idx xs = stamp "finding first match in index" (aux t idx xs)
   where
+    {-# INLINE aux #-}
     aux !_ !_ _ | False = __
     aux _ Nil rest = rest
-    aux t Index{size = size} rest
-      | lenList t < size = rest
-    aux Empty Index{here = here} rest = {-# SCC "try_here" #-} try here rest
-    aux t (Singleton u x) rest
-      | isJust (matchList u t) = {-# SCC "try_singleton" #-} try [x] rest
-      | otherwise = rest
-    aux t@(ConsSym (Fun (MkFun n _) _) ts) Index{fun = fun, var = var} rest =
-      case var of
-        Nil -> aux ts (fun ! n) rest
-        _   -> aux ts (fun ! n) (aux us var rest)
+    aux t Index{size = size, prefix = prefix} rest
+      | lenList t < size + lenList prefix = rest
+    aux t Index{..} rest =
+      pref t prefix here fun var rest
+
+    pref !_ !_ _ _ _ _ | False = __
+    pref Empty Empty here _ _ rest = try here rest
+    pref Empty _ _ _ _ _ = __
+    pref (Cons _ ts) (Cons (Var _) us) here fun var rest =
+      pref ts us here fun var rest
+    pref (ConsSym (Fun f _) ts) (ConsSym (Fun g _) us) here fun var rest
+      | f == g = pref ts us here fun var rest
+    pref _ (Cons _ _) _ _ _ rest = rest
+    pref t@(ConsSym (Fun (MkFun n _) _) ts) Empty _ fun var rest =
+      aux us var (aux ts (fun ! n) rest)
       where
         Cons _ us = t
-    aux (Cons _ ts) Index{var = var} rest = aux ts var rest
+    pref (Cons _ ts) Empty _ _ var rest = aux ts var rest
 
     {-# INLINE try #-}
     try [] rest = rest
@@ -161,7 +183,6 @@ find t idx xs = stamp "finding first match in index" (aux t idx xs)
 
 elems :: Index a -> [a]
 elems Nil = []
-elems (Singleton _ x) = [e_value x]
 elems idx =
   Prelude.map e_value (here idx) ++
   concatMap elems (Prelude.map snd (toList (fun idx))) ++
