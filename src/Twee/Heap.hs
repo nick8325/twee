1c43e516222c417131a91d4991b3c9e31af4f773
Nick Smallbone <nick@smallbone.se>
2017-08-15 18:14:50 +0200

    Tune heap code a bit.

--- src/Twee/Heap.hs
+++ src/Twee/Heap.hs
@@ -1,26 +1,32 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, ScopedTypeVariables #-}
 -- Skew heaps.
 module Twee.Heap(
-  Heap, empty, insert, removeMin, fromList, mapMaybe, size) where
+  Heap, empty, insert, removeMin, mapMaybe, size) where
 
 data Heap a = Heap {-# UNPACK #-} !Int !(Heap1 a)
-data Heap1 a = Nil | Node !a !(Heap1 a) !(Heap1 a)
+data Heap1 a = Nil | Node a (Heap1 a) (Heap1 a)
 
-{-# INLINE merge #-}
+{-# INLINEABLE merge #-}
 merge :: Ord a => Heap a -> Heap a -> Heap a
 merge (Heap n1 h1) (Heap n2 h2) = Heap (n1+n2) (merge1 h1 h2)
 
 {-# INLINEABLE merge1 #-}
-merge1 :: Ord a => Heap1 a -> Heap1 a -> Heap1 a
-merge1 Nil h = h
-merge1 h Nil = h
-merge1 h1@(Node x1 l1 r1) h2@(Node x2 l2 r2)
-  | x1 <= x2 = Node x1 (merge1 r1 h2) l1
-  | otherwise = Node x2 (merge1 r2 h1) l2
+merge1 :: forall a. Ord a => Heap1 a -> Heap1 a -> Heap1 a
+merge1 = m1
+  where
+    -- For some reason using m1 improves the code generation...
+    m1 :: Heap1 a -> Heap1 a -> Heap1 a
+    m1 Nil h = h
+    m1 h Nil = h
+    m1 h1@(Node x1 l1 r1) h2@(Node x2 l2 r2)
+      | x1 <= x2 = (Node x1 $! m1 r1 h2) l1
+      | otherwise = (Node x2 $! m1 r2 h1) l2
 
+{-# INLINE unit #-}
 unit :: a -> Heap a
-unit x = Heap 1 (Node x Nil Nil)
+unit !x = Heap 1 (Node x Nil Nil)
 
+{-# INLINE empty #-}
 empty :: Heap a
 empty = Heap 0 Nil
 
@@ -35,26 +41,34 @@ removeMin (Heap n (Node x l r)) = Just (x, Heap (n-1) (merge1 l r))
 
 {-# INLINEABLE mapMaybe #-}
 mapMaybe :: Ord b => (a -> Maybe b) -> Heap a -> Heap b
-mapMaybe f (Heap _ h) = fromList (go h [])
+mapMaybe f (Heap _ h) = Heap (sz 0 h') h'
   where
-    go Nil xs = xs
-    go (Node x l r) xs =
+    sz !n Nil = n
+    sz !n (Node _ l r) = sz (sz (n+1) l) r
+
+    h' = go h
+
+    go Nil = Nil
+    go (Node x l r) =
       case f x of
-        Just !y -> y:go l (go r xs)
-        Nothing -> go l (go r xs)
+        Nothing -> merge1 l' r'
+        Just !y -> down y l' r'
+      where
+        !l' = go l
+        !r' = go r
 
-{-# INLINEABLE fromList #-}
-fromList :: Ord a => [a] -> Heap a
-fromList = mergeMany . map unit 
-
-{-# INLINEABLE mergeMany #-}
-mergeMany :: Ord a => [Heap a] -> Heap a
-mergeMany [] = empty
-mergeMany [h] = h
-mergeMany hs = mergeMany (mergePairs hs)
-  where
-    mergePairs (x:y:xs) = ((:) $! merge x y) $ mergePairs xs
-    mergePairs xs = xs
+    down x l@(Node y ll lr) r@(Node z rl rr)
+      | y < x && y <= z =
+        (Node y $! down x ll lr) r
+      | z < x && z <= y =
+        Node z l $! down x rl rr
+    down x Nil (Node y l r)
+      | y < x =
+        Node y Nil $! down x l r
+    down x (Node y l r) Nil
+      | y < x =
+        (Node y $! down x l r) Nil
+    down x l r = Node x l r
 
 {-# INLINE size #-}
 size :: Heap a -> Int
