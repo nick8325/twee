76f6b7152f71ddef51d74c5d5b76d38b6e934641
Nick Smallbone <nick@smallbone.se>
2017-08-28 00:29:00 +0200

    Disable connectedness in main program too.

--- executable/Main.hs
+++ executable/Main.hs
@@ -115,8 +115,8 @@ parseConfig =
       expert $
       inGroup "Critical pair joining heuristics" $
       bool "connectedness"
-        ["Test terms for subconnectedness (on by default)."]
-        True
+        ["Test terms for subconnectedness (off by default)."]
+        False
     set_join =
       expert $
       inGroup "Critical pair joining heuristics" $
